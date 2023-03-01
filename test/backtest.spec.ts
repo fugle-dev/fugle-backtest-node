ab1e745ae574f0575aabb62477bff490c203bd47
Chun-Kai Wang <chunkai1312@gmail.com>
2023-03-02 02:23:41 +0800

    test: backtest

--- test/backtest.spec.ts
+++ test/backtest.spec.ts
@@ -1,12 +1,102 @@
-import { Backtest } from '../src';
+import { Backtest, HistoricalData } from '../src';
+import { Plotting } from '../src/plotting';
 import { TestStrategy } from './test-strategy';
 
 describe('Backtest', () => {
-  describe('constructor()', () => {
-    it('should be instantiated', () => {
+  describe('#constructor()', () => {
+    beforeAll(() => {
+      jest.spyOn(console, 'warn').mockImplementation(() => {});
+    });
+
+    it('should create Backtest instance', () => {
+      const data = require('./fixtures/2330.json');
+      const backtest = new Backtest(data, TestStrategy);
+      expect(backtest).toBeInstanceOf(Backtest);
+    });
+
+    it('should create Backtest instance with options', () => {
       const data = require('./fixtures/2330.json');
       const backtest = new Backtest(data, TestStrategy, { cash: 1000000 });
       expect(backtest).toBeInstanceOf(Backtest);
+    });
+
+    it('should print warning when some prices are larger than initial cash value', () => {
+      const data = require('./fixtures/2330.json');
+      const backtest = new Backtest(data, TestStrategy, { cash: 10 });
+      expect(backtest).toBeInstanceOf(Backtest);
+      expect(console.warn).toBeCalledWith('Some prices are larger than initial cash value.');
+    });
+
+    it('should throw error when Strategy is invalid', () => {
+      expect(() => {
+        const data = require('./fixtures/2330.json');
+        // @ts-ignore
+        new Backtest(data, {});
+      }).toThrow(TypeError);
+    });
+
+    it('should throw error when data is empty', () => {
+      expect(() => {
+        const data: HistoricalData = [];
+        new Backtest(data, TestStrategy);
+      }).toThrow(TypeError);
+    });
+
+    it('should throw error when some data fields are missing', () => {
+      expect(() => {
+        const data = [{ date: '2023-01-03' }];
+        // @ts-ignore
+        new Backtest(data, TestStrategy);
+      }).toThrow(TypeError);
+    });
+  });
+
+  describe('.run()', () => {
+    it('should run backtest and generate results', () => {
+      const data = require('./fixtures/2330.json');
+      const backtest = new Backtest(data, TestStrategy);
+      expect(backtest.results).toBeUndefined();
+      expect(backtest.run()).toBeInstanceOf(Backtest);
+      expect(backtest.results).toBeDefined();
+    });
+  });
+
+  describe('.print()', () => {
+    it('should print the results of the backtest run', () => {
+      const data = require('./fixtures/2330.json');
+      const backtest = new Backtest(data, TestStrategy).run();
+      // @ts-ignore
+      backtest._stats.results.print = jest.fn();
+      expect(backtest.print()).toBeInstanceOf(Backtest);
+      // @ts-ignore
+      expect(backtest._stats.results.print).toBeCalled();
+    });
+
+    it('should throw error when missing results', () => {
+      expect(() => {
+        const data = require('./fixtures/2330.json');
+        const backtest = new Backtest(data, TestStrategy);
+        backtest.print();
+      }).toThrow(Error);
+    });
+  });
+
+  describe('.plot()', () => {
+    it('should plot the equity curve of the backtest run', () => {
+      const data = require('./fixtures/2330.json');
+      const backtest = new Backtest(data, TestStrategy).run();
+      Plotting.prototype.plot = jest.fn();
+      expect(backtest.plot()).toBeInstanceOf(Backtest);
+      // @ts-ignore
+      expect(Plotting.prototype.plot).toBeCalled();
+    });
+
+    it('should throw error when missing results', () => {
+      expect(() => {
+        const data = require('./fixtures/2330.json');
+        const backtest = new Backtest(data, TestStrategy);
+        backtest.plot();
+      }).toThrow(Error);
     });
   });
 });
