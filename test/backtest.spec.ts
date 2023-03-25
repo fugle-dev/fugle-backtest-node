93f5f99b6860a0731f0528388c979f039887b949
Chun-Kai Wang <chunkai1312@gmail.com>
2023-03-25 22:25:59 +0800

    test: backtest

--- test/backtest.spec.ts
+++ test/backtest.spec.ts
@@ -1,35 +1,37 @@
-import { Backtest, HistoricalData } from '../src';
+import { Backtest } from '../src';
 import { Stats } from '../src/stats';
-import { TestStrategy } from './test-strategy';
+import { SmaCross } from './sma-cross.strategy';
 
 describe('Backtest', () => {
-  describe('#constructor()', () => {
-    beforeAll(() => {
+  const data = require('./fixtures/2330.json');
+
+  describe('constructor()', () => {
+    beforeEach(() => {
       jest.spyOn(console, 'warn').mockImplementation(() => {});
     });
 
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
     it('should create Backtest instance', () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy);
+      const backtest = new Backtest(data, SmaCross);
       expect(backtest).toBeInstanceOf(Backtest);
     });
 
     it('should create Backtest instance with options', () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy, { cash: 1000000 });
+      const backtest = new Backtest(data, SmaCross, { cash: 1000000 });
       expect(backtest).toBeInstanceOf(Backtest);
     });
 
     it('should print warning when some prices are larger than initial cash value', () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy, { cash: 10 });
+      const backtest = new Backtest(data, SmaCross, { cash: 100 });
       expect(backtest).toBeInstanceOf(Backtest);
       expect(console.warn).toBeCalledWith('Some prices are larger than initial cash value.');
     });
 
     it('should throw error when Strategy is invalid', () => {
       expect(() => {
-        const data = require('./fixtures/2330.json');
         // @ts-ignore
         new Backtest(data, {});
       }).toThrow(TypeError);
@@ -37,24 +39,24 @@ describe('Backtest', () => {
 
     it('should throw error when data is empty', () => {
       expect(() => {
-        const data: HistoricalData = [];
-        new Backtest(data, TestStrategy);
+        new Backtest([], SmaCross);
       }).toThrow(TypeError);
     });
 
     it('should throw error when some data fields are missing', () => {
       expect(() => {
-        const data = [{ date: '2023-01-03' }];
         // @ts-ignore
-        new Backtest(data, TestStrategy);
+        new Backtest([{ date: '2023-01-03' }], SmaCross);
       }).toThrow(TypeError);
     });
   });
 
   describe('.run()', () => {
     it('should run backtest and generate stats', async () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy);
+      const backtest = new Backtest(data, SmaCross, {
+        cash: 1000000,
+        tradeOnClose: true,
+      });
       expect(backtest.stats).toBeUndefined();
       await backtest.run();
       expect(backtest.stats).toBeDefined();
@@ -63,18 +65,27 @@ describe('Backtest', () => {
 
   describe('.optimize()', () => {
     it('should optimize strategy parameters', async () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy);
+      const backtest = new Backtest(data, SmaCross);
       expect(backtest.stats).toBeUndefined();
-      await backtest.optimize({ params: { period: [5, 20, 60] } });
+      await backtest.optimize({ params: { n1: [5, 10, 20], n2: [60, 120, 240] } });
       expect(backtest.stats).toBeDefined();
+    });
+
+    it('should throw error when no parameters provided', async () => {
+      const backtest = new Backtest(data, SmaCross);
+      // @ts-ignore
+      await expect(() => backtest.optimize({})).rejects.toThrow();
+    });
+
+    it('should throw error when parameters are empty', async () => {
+      const backtest = new Backtest(data, SmaCross);
+      await expect(() => backtest.optimize({ params: {} })).rejects.toThrow();
     });
   });
 
   describe('.print()', () => {
     it('should print the results of the backtest run', async () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy);
+      const backtest = new Backtest(data, SmaCross);
       await backtest.run();
       Stats.prototype.print = jest.fn();
       expect(backtest.print()).toBeInstanceOf(Backtest);
@@ -82,18 +93,14 @@ describe('Backtest', () => {
     });
 
     it('should throw error when missing results', () => {
-      expect(() => {
-        const data = require('./fixtures/2330.json');
-        const backtest = new Backtest(data, TestStrategy);
-        backtest.print();
-      }).toThrow(Error);
+      const backtest = new Backtest(data, SmaCross);
+      expect(() => backtest.print()).toThrow(Error);
     });
   });
 
   describe('.plot()', () => {
     it('should plot the equity curve of the backtest run', async () => {
-      const data = require('./fixtures/2330.json');
-      const backtest = new Backtest(data, TestStrategy);
+      const backtest = new Backtest(data, SmaCross);
       await backtest.run();
       Stats.prototype.plot = jest.fn();
       expect(backtest.plot()).toBeInstanceOf(Backtest);
@@ -101,11 +108,8 @@ describe('Backtest', () => {
     });
 
     it('should throw error when missing results', () => {
-      expect(() => {
-        const data = require('./fixtures/2330.json');
-        const backtest = new Backtest(data, TestStrategy);
-        backtest.plot();
-      }).toThrow(Error);
+      const backtest = new Backtest(data, SmaCross);
+      expect(() => backtest.plot()).toThrow(Error);
     });
   });
 });
